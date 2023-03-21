f0f0588cf4d034cbef413cafb0fa1982adf7a734
Precious Akams <preciousakams@yahoo.com>
2023-03-21 21:35:21 +0100

    axios removed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import store from './Redux/configureStore';
 import App from './App';
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
 );
